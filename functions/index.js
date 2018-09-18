720e205aacf418ed4eb52952d37f86f8feb7d005
Adnan <muh.adnansuryaazis@gmail.com>
2018-09-18 20:06:17 +0800

    add ejs and bootstrap template

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
 const functions = require('firebase-functions');
+//include library expressjs setelah install di folder functions : npm i --save firebase-functions
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//inisialisasi apps expressjs
+const app = express();
+
+//set template engine ke ejs setelah install di folder functions : npm install ejs -save
+app.set('view engine', 'ejs');
+//set folder static ke public untuk menyimpan file statis
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res) {
+  res.render('index');
+});
+
+app.get('/admin/dashboard', function(req,res) {
+  res.render('admin/index');
+});
+
+app.get('/admin/', function(req,res) {
+  res.redirect('/admin/dashboard');
+});
+
+
+app.get('/admin/kategori', function(req,res) {
+  res.render('admin/kategori');
+});
+
+
+exports.apps = functions.https.onRequest(app);
